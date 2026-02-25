b7fac9552ce798823da8050c5ed2847b7aafa424
AlysonMatheus <alyson2000@live.com>
2026-02-25 16:45:29 -0300

    Update PedidoRequestDTO.java

--- src/main/java/alysondev/lanchonete/dtos/request/PedidoRequestDTO.java
+++ src/main/java/alysondev/lanchonete/dtos/request/PedidoRequestDTO.java
@@ -1,19 +1,14 @@
 package alysondev.lanchonete.dtos.request;
 
-import alysondev.lanchonete.entity.ItensPedido;
-import alysondev.lanchonete.entity.Produto;
+import alysondev.lanchonete.entity.ItensPedido; 
 import alysondev.lanchonete.enums.Pagamento;
-import jakarta.validation.constraints.NotBlank;
 
-import java.util.Date;
+
 import java.util.List;
 
 public record PedidoRequestDTO(Long idPedido,
                                Long idCliente,
                                Pagamento pagamento,
-                               Double precoTotal,
-                               Date dataHora,
                                Long idEndereco,
-
                                List<ItensPedido> pedidoList) {
 }
